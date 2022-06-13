986d3b6763f5a582d438fa46d1c18775e7530e98
Rathaur <rishabh.rathaur@mind.motherson.com>
2022-06-13 17:35:17 +0530

    Maven App

---
+++ src/main/java/mind/dna/my_app/App.java
@@ -0,0 +1,13 @@
+package mind.dna.my_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello [RISHABH RATHAUR]");
+    }
+}
